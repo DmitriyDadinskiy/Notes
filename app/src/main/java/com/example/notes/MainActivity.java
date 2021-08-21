507f512b5665e51fba71399b8bbc96e92e87a1b6
Dmitrii <dim1928@yandex.ru>
2021-08-22 02:48:51 +0300

    Homework for lesson 7

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
     }
 }
